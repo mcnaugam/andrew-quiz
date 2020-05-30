2809c80e59f785b6bcddb7209616beea64f45487
Andrew McNaught <andrewmcnaught2@gmail.com>
2020-05-30 11:59:22 +0100

    Updates

---
+++ precache-manifest.e84ab3dfdeec6827f46bebab5e88db17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c509720e87c12cd0da7f9ba8c22d675",
+    "url": "/andrew-quiz/index.html"
+  },
+  {
+    "revision": "d644112062ce65db5858",
+    "url": "/andrew-quiz/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "509fd599ef4d7d4b1168",
+    "url": "/andrew-quiz/static/css/main.72832c22.chunk.css"
+  },
+  {
+    "revision": "d644112062ce65db5858",
+    "url": "/andrew-quiz/static/js/2.a67b82d6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/andrew-quiz/static/js/2.a67b82d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509fd599ef4d7d4b1168",
+    "url": "/andrew-quiz/static/js/main.790fc599.chunk.js"
+  },
+  {
+    "revision": "76bb8993bdf7206c04b3",
+    "url": "/andrew-quiz/static/js/runtime-main.c09b67c8.js"
+  },
+  {
+    "revision": "b739fd313524b464d507ea621b657de4",
+    "url": "/andrew-quiz/static/media/14876504_correct_by_robotsound_preview.b739fd31.mp3"
+  },
+  {
+    "revision": "66615ba47ca38e6a4329077f240205ee",
+    "url": "/andrew-quiz/static/media/23205030_game-show-buzzer-wrong-answer_by_floraphonic_preview.66615ba4.mp3"
+  }
+]);
