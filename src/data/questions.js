82c725b8e7da39cd31c399f4b96a5c82c70a02d5
Andrew McNaught <andrewmcnaught2@gmail.com>
2020-05-30 11:55:37 +0100

    First commit

---
+++ src/data/questions.js
@@ -0,0 +1,629 @@
+export const SET1 = [
+  {
+    theme: "Politics",
+    question:
+      "Last 20 American Presidents from 1901 to present day, earliest to recent",
+    answers: [
+      {
+        answer: "Theodore Roosevelt",
+        value: "1901 - 1909",
+      },
+      {
+        answer: "William Howard Taft",
+        value: "1909 - 1913",
+      },
+      {
+        answer: "Woodrow Wilson",
+        value: "1913 - 1921",
+      },
+      {
+        answer: "Warren G. Harding",
+        value: "1921 - 1923",
+      },
+      {
+        answer: "Calvin Coolidge",
+        value: "1923 - 1929",
+      },
+      {
+        answer: "Herbert Hoover",
+        value: "1929 - 1933",
+      },
+      {
+        answer: "Franklin D. Roosevelt",
+        value: "1933 - 1945",
+      },
+      {
+        answer: "Harry S. Truman",
+        value: "1945 - 1953",
+      },
+      {
+        answer: "Dwight D. Eisenhower",
+        value: "1953 - 1961",
+      },
+      {
+        answer: "John F. Kennedy",
+        value: "1961 - 1963",
+      },
+      {
+        answer: "Lyndon B. Johnson",
+        value: "1963 - 1969",
+      },
+      {
+        answer: "Richard Nixon",
+        value: "1969 - 1974",
+      },
+      {
+        answer: "Gerald Ford",
+        value: "1974 - 1977",
+      },
+      {
+        answer: "Jimmy Carter",
+        value: "1977 - 1981",
+      },
+      {
+        answer: "Ronald Reagan",
+        value: "1981 - 1989",
+      },
+      {
+        answer: "George H. W. Bush",
+        value: "1989 - 1993",
+      },
+      {
+        answer: "Bill Clinton",
+        value: "1993 - 2001",
+      },
+      {
+        answer: "George W. Bush",
+        value: "2001 - 2009",
+      },
+      {
+        answer: "Barack Obama",
+        value: "2009 - 2017",
+      },
+      {
+        answer: "Donald Trump",
+        value: "2017 - ",
+      },
+    ],
+  },
+  {
+    theme: "Sport",
+    question: "Top 10 Longest serving English Premier league managers",
+    answers: [
+      {
+        answer: "Arsene Wenger",
+        value: "Arsenal | October 1996 - May 2018",
+      },
+      {
+        answer: "Sir Alex Ferguson",
+        value: "Man United | August 1992 - May 2013",
+      },
+      {
+        answer: "David Moyes",
+        value: "Everton | March 2002 - May 2013",
+      },
+      {
+        answer: "Harry Redknapp",
+        value: "West Ham United | August 1994 - May 2001",
+      },
+      {
+        answer: "Rafael Benitez",
+        value: "Liverpool | August 2004 - June 2010",
+      },
+      {
+        answer: "Alan Curbishley",
+        value: "Charlton Athletic | August 2000 - May 2006",
+      },
+      {
+        answer: "Sam Allardyce",
+        value: "Bolton Wanderers | August 2001 - April 2007",
+      },
+      {
+        answer: "Gerard Houllier",
+        value: "Liverpool | November 1998 - May 2004",
+      },
+      {
+        answer: "Jim Smith",
+        value: "Derby County | August 1996 - October 2001",
+      },
+      {
+        answer: "Sir Bobby Robson",
+        value: "Newcastle United | September 1999 - August 2004",
+      },
+    ],
+  },
+  {
+    theme: "Film",
+    question:
+      "Top 15 highest grossing films of the 90s (worldwide and unadjusted)",
+    answers: [
+      {
+        answer: "Titanic",
+        value: "$1,843,201,268",
+      },
+      {
+        answer: "Star Wars: Episode I – The Phantom Menace",
+        value: "$924,317,558",
+      },
+      {
+        answer: "Jurassic Park",
+        value: "$914,691,118",
+      },
+      {
+        answer: "Independence Day",
+        value: "$817,400,891",
+      },
+      {
+        answer: "The Lion King",
+        value: "$763,455,561",
+      },
+      {
+        answer: "Forrest Gump",
+        value: "$677,387,716",
+      },
+      {
+        answer: "The Sixth Sense",
+        value: "$672,806,292",
+      },
+      {
+        answer: "The Lost World: Jurassic Park",
+        value: "$618,638,999",
+      },
+      {
+        answer: "Men in Black",
+        value: "$589,390,539",
+      },
+      {
+        answer: "Armageddon",
+        value: "$553,709,788",
+      },
+      {
+        answer: "Terminator 2: Judgment Day",
+        value: "$519,843,345",
+      },
+      {
+        answer: "Ghost",
+        value: "$505,702,588",
+      },
+      {
+        answer: "Aladdin",
+        value: "$504,050,219",
+      },
+      {
+        answer: "Twister",
+        value: "$494,471,524",
+      },
+      {
+        answer: "Toy Story 2",
+        value: "$485,015,179",
+      },
+    ],
+  },
+  {
+    theme: "Geography",
+    question:
+      "Top 15 capital city populations by official capital area, not wider metropolitan/urban district",
+    answers: [
+      {
+        answer: "Beijing",
+        answer2: "China",
+        value: "21,542,000",
+      },
+
+      {
+        answer: "New Delhi",
+        answer2: "India",
+        value: "14,200,004",
+      },
+      {
+        answer: "Tokyo",
+        answer2: "Japan",
+        value: "13,929,286",
+      },
+      {
+        answer: "Moscow",
+        answer2: "Russia",
+        value: "12,506,468",
+      },
+      {
+        answer: "Kinshasa",
+        answer2: "DR Congo",
+        value: "11,855,000",
+      },
+      {
+        answer: "Jakarta",
+        answer2: "Indonesia",
+        value: "10,075,310",
+      },
+      {
+        answer: "Seoul",
+        answer2: "South Korea",
+        value: "9,838,892",
+      },
+      {
+        answer: "Cairo",
+        answer2: "Egypt",
+        value: "9,500,000",
+      },
+      {
+        answer: "Mexico City",
+        answer2: "Mexico",
+        value: "8,918,653",
+      },
+      {
+        answer: "London",
+        answer2: "United Kingdom",
+        value: "8,908,081",
+      },
+      {
+        answer: "Dhaka",
+        answer2: "Bangladesh",
+        value: "8,906,039",
+      },
+      {
+        answer: "Lima",
+        answer2: "Peru",
+        value: "8,852,000",
+      },
+      {
+        answer: "Tehran",
+        answer2: "Iran",
+        value: "8,693,706",
+      },
+      {
+        answer: "Bangkok",
+        answer2: "Thailand",
+        value: "8,305,218",
+      },
+      {
+        answer: "Hanoi",
+        answer2: "Vietnam",
+        value: "7,781,631",
+      },
+    ],
+  },
+  {
+    theme: "Music",
+    question:
+      "Top 20 best selling artists by total certified album units sold in the US including streaming",
+
+    answers: [
+      {
+        answer: "The Beatles",
+        value: "183 million units",
+      },
+      {
+        answer: "Garth Brooks",
+        value: "156 million units",
+      },
+      {
+        answer: "Elvis Presley",
+        value: "146.5 million units",
+      },
+      {
+        answer: "Eagles",
+        value: "120 million units",
+      },
+      {
+        answer: "Led Zeppelin",
+        value: "111.5 million units",
+      },
+      {
+        answer: "Billy Joel",
+        value: "84.5 million units",
+      },
+      {
+        answer: "Michael Jackson",
+        value: "84 million units",
+      },
+      {
+        answer: "Elton John",
+        value: "79 million units",
+      },
+      {
+        answer: "AC DC",
+        value: "75 million units",
+      },
+      {
+        answer: "Pink Floyd",
+        value: "75 million units",
+      },
+      {
+        answer: "George Strait",
+        value: "69 million units",
+      },
+      {
+        answer: "Barbra Streisand",
+        value: "68.5 million units",
+      },
+      {
+        answer: "Aerosmith",
+        value: "66.5 million units",
+      },
+      {
+        answer: "Mariah Carey",
+        value: "66.5 million units",
+      },
+      {
+        answer: "The Rolling Stones",
+        value: "66.5 million units",
+      },
+      {
+        answer: "Bruce Springsteen",
+        value: "65.5 million units",
+      },
+      {
+        answer: "Madonna",
+        value: "64.5 million units",
+      },
+      {
+        answer: "Metallica",
+        value: "63 million units",
+      },
+      {
+        answer: "Whitney Houston",
+        value: "59 million units",
+      },
+
+      {
+        answer: "Van Halen",
+        value: "56.5 million units",
+      },
+    ],
+  },
+  {
+    theme: "Sport",
+    question: "Top 15 footballers by total accumulated transfer fees",
+    condition: "Highest to lowest - more points for higher total transfer fee",
+    answers: [
+      {
+        answer: "Neymar",
+        value: "£279.18m",
+      },
+      {
+        answer: "Cristiano Ronaldo",
+        value: "£207.00m",
+      },
+      {
+        answer: "Romelu Lukaku",
+        value: "£183.20m",
+      },
+      {
+        answer: "Alvaro Morata",
+        value: "£161.10m",
+      },
+      {
+        answer: "Angel Di Maria",
+        value: "£161.10m",
+      },
+      {
+        answer: "Antoine Griezmann",
+        value: "£156.60m",
+      },
+      {
+        answer: "Philippe Coutinho",
+        value: "£153.27m",
+      },
+      {
+        answer: "Zlatan Ibrahimovic",
+        value: "£152.19m",
+      },
+      {
+        answer: "Gonzalo Higuain",
+        value: "£143.10m",
+      },
+      {
+        answer: "Kylian Mbappe",
+        value: "£130.50m",
+      },
+      {
+        answer: "James Rodriguez",
+        value: "£126.57m",
+      },
+      {
+        answer: "Ousmane Dembele",
+        value: "£126.00m",
+      },
+      {
+        answer: "Eden Hazard",
+        value: "£121.50m",
+      },
+      {
+        answer: "Paulinho",
+        value: "£114.17m",
+      },
+      {
+        answer: "Joao Felix",
+        value: "£113.40m",
+      },
+    ],
+  },
+
+  {
+    theme: "Cars",
+    question:
+      "Top 15 car marques by total new passenger vehicle registrations in the UK 2019",
+    answers: [
+      {
+        answer: "Ford",
+        value: "236,137",
+      },
+      {
+        answer: "Volkswagen",
+        value: "200,771",
+      },
+      {
+        answer: "Mercedes-Benz",
+        value: "171,823",
+      },
+      {
+        answer: "BMW",
+        value: "169,753",
+      },
+      {
+        answer: "Vauxhall",
+        value: "159,830",
+      },
+      {
+        answer: "Audi",
+        value: "138,924",
+      },
+      {
+        answer: "Toyota",
+        value: "105,192",
+      },
+      {
+        answer: "Kia",
+        value: "97,323",
+      },
+      {
+        answer: "Nissan",
+        value: "92,372",
+      },
+
+      {
+        answer: "Hyundai",
+        value: "83,284",
+      },
+      {
+        answer: "Peugeot",
+        value: "80,851",
+      },
+      {
+        answer: "Land Rover",
+        value: "76,546",
+      },
+      {
+        answer: "Skoda",
+        value: "75,053",
+      },
+      {
+        answer: "SEAT",
+        value: "68,798",
+      },
+
+      {
+        answer: "MINI",
+        value: "64,884",
+      },
+    ],
+  },
+  {
+    theme: "Board games",
+    question: "Top 15 highest selling board games of all time",
+    answers: [
+      {
+        answer: "Chess",
+      },
+      {
+        answer: "Draughts",
+      },
+      {
+        answer: "Backgammon",
+      },
+      {
+        answer: "Monopoly",
+      },
+      {
+        answer: "Scrabble",
+      },
+      {
+        answer: "Cluedo",
+      },
+      {
+        answer: "Trivial pursuit",
+      },
+      {
+        answer: "Battleship",
+      },
+      {
+        answer: "Candy land",
+      },
+      {
+        answer: "The Game of Life",
+      },
+      {
+        answer: "Rummikub",
+      },
+      {
+        answer: "Othello",
+      },
+      {
+        answer: "Pictionary",
+      },
+      {
+        answer: "Risk",
+      },
+      {
+        answer: "Catan",
+      },
+    ],
+  },
+
+  {
+    theme: "Geography",
+    question: "Top 15 smallest European countries/territories by population",
+    answers: [
+      {
+        answer: "Vatican City",
+        value: "799",
+      },
+      {
+        answer: "Aland Islands (Finland)",
+        value: "29,489",
+      },
+      {
+        answer: "San Marino",
+        value: "33,407",
+      },
+      {
+        answer: "Gibraltar (UK)",
+        value: "33,573",
+      },
+      {
+        answer: "Liechtenstein",
+        value: "38,201",
+      },
+      {
+        answer: "Monaco",
+        value: "38,300",
+      },
+      {
+        answer: "Faroe Islands (Denmark)",
+        value: "51,237",
+      },
+      {
+        answer: "Guernsey (UK)",
+        value: "62,063",
+      },
+      {
+        answer: "Andorra",
+        value: "74,794",
+      },
+      {
+        answer: "Isle of Man (UK)",
+        value: "83,314",
+      },
+      {
+        answer: "Jersey (UK)",
+        value: "105,500",
+      },
+      {
+        answer: "Iceland",
+        value: "355,620",
+      },
+      {
+        answer: "Malta",
+        value: "475,701",
+      },
+      {
+        answer: "Luxembourg",
+        value: "626,108",
+      },
+      {
+        answer: "Montenegro",
+        value: "622,359",
+      },
+    ],
+  },
+];
